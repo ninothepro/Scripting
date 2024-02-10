95b04e905636de32b4478cfd87cfe37ee5c8bd29
ninothepro <81441896+ninothepro@users.noreply.github.com>
2024-02-10 15:28:15 +0200

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    //Prints (Outputs) "Hello World" to the console
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
